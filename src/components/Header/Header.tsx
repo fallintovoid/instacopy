cab9f4fee0d76ed8fb3798465ca857f6d74f9141
Denis Maliyshock <denis.malyshok@ramax.ru>
2022-10-22 14:52:03 +0200

    input changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,46 +1,53 @@
-import React from 'react'
-import { ReactComponent as Logo } from '../../images/logo.svg';
-import { RiProfileFill, RiProfileLine } from 'react-icons/ri'
-import { AiOutlineHome, AiFillHome } from 'react-icons/ai'
-import { Link, useLocation } from 'react-router-dom';
+import React, { useCallback } from "react";
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import { RiProfileFill, RiProfileLine } from "react-icons/ri";
+import { AiOutlineHome, AiFillHome } from "react-icons/ai";
+import { Link, useLocation } from "react-router-dom";
 
-import s from './Header.module.scss'
-import Input from '../Input/Input';
+import s from "./Header.module.scss";
+import Input from "../Input/Input";
 
 const Header = () => {
-    const { pathname } = useLocation()
+  const { pathname } = useLocation();
+  const handleChange = useCallback(() => {
+    // можно делать штуки тут
+  }, []);
 
-    return (
-        <header className={s.header}>
-            <div className={s.header_inner}>
-                <Link className={s.logo} to='/'>
-                    <Logo/>
-                </Link>
-                <div className={s.searchbar}>
-                    {/*TODO: сделать инпут компонентом*/}
-                    <Input placeholder='Search'/>
-                </div>
-                <menu className={s.menu}>
-                    <ul className={s.list}>
-                        <li>
-                            <Link className={s.link} to='/'>
-                                {pathname === '/'
-                                    ? <AiFillHome className={s.icon}/>
-                                    : <AiOutlineHome className={s.icon}/>}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className={s.link} to='/profile'>
-                                {pathname === '/profile'
-                                    ? <RiProfileFill className={s.icon}/>
-                                    : <RiProfileLine className={s.icon}/>}
-                            </Link>
-                        </li>
-                    </ul>
-                </menu>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className={s.header}>
+      <div className={s.header_inner}>
+        <Link className={s.logo} to="/">
+          <Logo />
+        </Link>
+        <div className={s.searchbar}>
+          {/*TODO: сделать инпут компонентом*/}
+          <Input onChange={handleChange} placeholder="Search" />
+        </div>
+        <menu className={s.menu}>
+          <ul className={s.list}>
+            <li>
+              <Link className={s.link} to="/">
+                {pathname === "/" ? (
+                  <AiFillHome className={s.icon} />
+                ) : (
+                  <AiOutlineHome className={s.icon} />
+                )}
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} to="/profile">
+                {pathname === "/profile" ? (
+                  <RiProfileFill className={s.icon} />
+                ) : (
+                  <RiProfileLine className={s.icon} />
+                )}
+              </Link>
+            </li>
+          </ul>
+        </menu>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
